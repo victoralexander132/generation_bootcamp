ee91026fbdb76ef9e67e7bc1d264e57e1c4f3e62
vic <alexandervictor550@gmail.com>
2022-04-27 15:08:45 -0500

    addEventListener

--- JavaScript/data_types/assets/js/jonathan.js
+++ JavaScript/data_types/assets/js/jonathan.js
@@ -3,10 +3,22 @@
 const yearActual = 2022;
 console.log(`Tienes ${yearActual - yearBirth} años`); */
 
-// Nivel 2
+// Nivel 2 Opción 1
+// const yearActual = 2022;
+
+// function calcular() {
+//   const yearBirth = parseInt(document.getElementById('year').value);
+//   console.log(`Tienes ${yearActual - yearBirth} años`);
+// }
+
+// Nivel 2 Opción 2
 const yearActual = 2022;
+const boton = document.getElementById('calcular');
 
 function calcular() {
   const yearBirth = parseInt(document.getElementById('year').value);
   console.log(`Tienes ${yearActual - yearBirth} años`);
 }
+
+
+boton.addEventListener('click', calcular)
