2c105caef35434b91e11afcc4b8859f509541167
vic <alexandervictor550@gmail.com>
2022-04-27 15:41:57 -0500

    Nivel 3 con split

--- JavaScript/data_types/assets/js/jonathan.js
+++ JavaScript/data_types/assets/js/jonathan.js
@@ -12,13 +12,36 @@ console.log(`Tienes ${yearActual - yearBirth} años`); */
 // }
 
 // Nivel 2 Opción 2
+// const yearActual = 2022;
+// const boton = document.getElementById('calcular');
+
+// function calcular() {
+//   const yearBirth = parseInt(document.getElementById('year').value);
+//   console.log(`Tienes ${yearActual - yearBirth} años`);
+// }
+
+
+// boton.addEventListener('click', calcular)
+
+// Nivel 3
+
 const yearActual = 2022;
-const boton = document.getElementById('calcular');
+const monthActual = 4;
+const dayActual = 27;
 
-function calcular() {
-  const yearBirth = parseInt(document.getElementById('year').value);
-  console.log(`Tienes ${yearActual - yearBirth} años`);
+function mostrarDatos() {
+  const birth = document.getElementById('year').value;
+
+  const ymd = birth.split('-');
+  yyyy = parseInt(ymd[0]);
+  mm = parseInt(ymd[1]);
+  dd = parseInt(ymd[2]);
+  if (mm <= monthActual && dd <= dayActual) {
+    console.log("Ya cumpliste años")
+    console.log(`Tienes ${yearActual - yyyy} años`)
+  } else {
+    console.log(`Tienes ${yearActual - yyyy - 1} años`)
+    console.log("Todavía no cumpliste años")
+    
+  }
 }
-
-
-boton.addEventListener('click', calcular)
